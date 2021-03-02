fe3ebf43932e2ed05c2651db4ec122da7e0d1d5d
zjbztianya <spcjv5@gmail.com>
2021-03-02 23:29:47 +0800

    Add md5 hash

--- hashkit/hash.go
+++ hashkit/hash.go
@@ -1,6 +1,7 @@
 package hashkit
 
 import (
+	"crypto/md5"
 	"github.com/spaolacci/murmur3"
 	"hash/fnv"
 )
@@ -17,4 +18,12 @@ func Fnv32(data []byte) uint32 {
 	f := fnv.New32()
 	f.Write(data)
 	return f.Sum32()
+}
+
+func Md5(key []byte) uint32 {
+	m := md5.New()
+	m.Write(key)
+	results := m.Sum(nil)
+	return (uint32(results[3]&0xFF) << 24) | (uint32(results[2]&0xFF) << 16) |
+		(uint32(results[1]&0xFF) << 8) | (uint32(results[0]) & 0xFF)
 }
