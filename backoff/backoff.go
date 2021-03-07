4b96ce97693f5fc17c4b9a939513895f91b9926e
zjbztianya <spcjv5@gmail.com>
2021-03-07 12:54:59 +0800

    Add exponential backoff algorithm with jitter

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -1,1 +1,42 @@
 package backoff
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Config struct {
+	BaseDelay  time.Duration
+	MaxDelay   time.Duration
+	Multiplier float64
+	Jitter     float64
+}
+
+// Exponential implements exponential backoff algorithm as defined in
+// https://aws.amazon.com/cn/blogs/architecture/exponential-backoff-and-jitter/
+type Exponential struct {
+	cfg *Config
+}
+
+func (e *Exponential) BackOff(retries int) time.Duration {
+	if retries == 0 {
+		return e.cfg.BaseDelay
+	}
+
+	backoff, maxDelay := float64(e.cfg.BaseDelay), float64(e.cfg.MaxDelay)
+	//for loop performance better than math.Pow
+	for retries > 0 && backoff < maxDelay {
+		backoff *= e.cfg.Multiplier
+		retries--
+	}
+	if backoff > maxDelay {
+		backoff = maxDelay
+	}
+
+	// backoff range:[backoff*(1-jitter),backoff*(1+jitter))
+	backoff = backoff * (1 + e.cfg.Jitter*(rand.Float64()*2-1))
+	if backoff < 0 {
+		backoff = 0
+	}
+	return time.Duration(backoff)
+}
--- backoff/backoff_test.go
+++ backoff/backoff_test.go
@@ -1,1 +1,30 @@
 package backoff
+
+import (
+	"testing"
+	"time"
+)
+
+func getExpBackoff() *Exponential {
+	cfg := &Config{
+		BaseDelay:  time.Second,
+		MaxDelay:   120 * time.Second,
+		Multiplier: 2,
+		Jitter:     0.2,
+	}
+	return &Exponential{cfg: cfg}
+}
+
+func TestExponentialBackOff(t *testing.T) {
+	expBackoff := getExpBackoff()
+	for i := 0; i < 10; i++ {
+		t.Log(expBackoff.BackOff(i).Milliseconds())
+	}
+}
+
+func BenchmarkExponentialBackOff(b *testing.B) {
+	expBackoff := getExpBackoff()
+	for i := 0; i < b.N; i++ {
+		expBackoff.BackOff(i % 10)
+	}
+}
